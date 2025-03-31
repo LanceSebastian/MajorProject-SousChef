b893137853f14a685450459fc85a498d0317119a
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-03-31 07:26:19 +0100

    Fix(api): compatible versions
    Previously, kotlin version mismatched the firebase version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx.v190)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -61,7 +61,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.coil.compose)
     implementation(libs.datetime)
