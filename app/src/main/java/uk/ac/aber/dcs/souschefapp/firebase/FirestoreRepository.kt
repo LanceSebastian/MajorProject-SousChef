98e082fc19e928eb83982884e5beafe7f32c1b71
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-03-31 03:16:05 +0100

    Create File

---
+++ app/src/main/java/uk/ac/aber/dcs/souschefapp/firebase/FirestoreRepository.kt
@@ -0,0 +1,7 @@
+package uk.ac.aber.dcs.souschefapp.firebase
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreRepository {
+    private val db = FirebaseFirestore.getInstance()
+}
