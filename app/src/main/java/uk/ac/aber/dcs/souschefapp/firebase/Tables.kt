67edf2992b031c275e5a880b12e040be7950d236
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-03-31 02:45:48 +0100

    Add Tables for firestore

---
+++ app/src/main/java/uk/ac/aber/dcs/souschefapp/firebase/Tables.kt
@@ -0,0 +1,46 @@
+package uk.ac.aber.dcs.souschefapp.firebase
+
+data class Product(
+    val createdBy: String,
+    val name: String = "",
+    val price: Double = 1.0,
+)
+
+data class Recipe(
+    val recipeName: String,
+    val instructionList: List<String> = emptyList<String>(),
+    val tags: List<String> = emptyList<String>(), // Will need to think about this further
+    val isActive: Boolean = true
+)
+
+// Subcollection to Recipe
+data class Instruction(
+    val content: String,
+    val placement: Int = 0,
+)
+
+// Subcollection to Recipe
+data class Ingredient(
+    val ingredientId: Int = 0,
+    val recipeOwnerId: Int,
+    val name: String,
+    val description: String,
+    val quantity: Int,
+    val unit: String
+)
+
+// Subcollection to Recipe
+data class Note(
+    val content: String,
+    val timestamp: Long
+)
+
+data class Log(
+    val createdBy: String,
+    val timestamp: Long,
+    val rating: Int = 0,
+    val note: String = "",
+    val recipeIdList: List<Int> = emptyList(),
+    val productIdList: List<Int> = emptyList()
+)
+
