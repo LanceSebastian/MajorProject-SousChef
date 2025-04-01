9301fe7d41408c610efbd85e8e607e8ff103ac41
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-04-01 17:29:06 +0100

    Add(file): RecipeViewModel.kt

---
+++ app/src/main/java/uk/ac/aber/dcs/souschefapp/firebase/viewmodel/RecipeViewModel.kt
@@ -0,0 +1,16 @@
+package uk.ac.aber.dcs.souschefapp.firebase.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import uk.ac.aber.dcs.souschefapp.firebase.Recipe
+//import uk.ac.aber.dcs.souschefapp.firebase.FirebaseRepository
+//
+//class RecipeViewModel : ViewModel() {
+//    private val recipeRepository = RecipeRepository()
+//
+//    private var _recipe = MutableLiveData<Recipe>()
+//    var recipe: LiveData<Recipe> = _recipe
+//
+//
+//}
