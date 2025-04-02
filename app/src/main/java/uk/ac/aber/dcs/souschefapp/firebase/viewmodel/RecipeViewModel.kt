9a31eed21d88535e3bcbd4cc860e9c1bbfe7151f
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-04-02 22:31:30 +0100

    Feat: add view model that handles recipes

--- app/src/main/java/uk/ac/aber/dcs/souschefapp/firebase/viewmodel/RecipeViewModel.kt
+++ app/src/main/java/uk/ac/aber/dcs/souschefapp/firebase/viewmodel/RecipeViewModel.kt
@@ -1,16 +1,187 @@
 package uk.ac.aber.dcs.souschefapp.firebase.viewmodel
 
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.ListenerRegistration
+import kotlinx.coroutines.launch
 import uk.ac.aber.dcs.souschefapp.firebase.Recipe
-//import uk.ac.aber.dcs.souschefapp.firebase.FirebaseRepository
-//
-//class RecipeViewModel : ViewModel() {
-//    private val recipeRepository = RecipeRepository()
-//
-//    private var _recipe = MutableLiveData<Recipe>()
-//    var recipe: LiveData<Recipe> = _recipe
-//
-//
-//}
+import uk.ac.aber.dcs.souschefapp.firebase.RecipeRepository
+
+class RecipeViewModel : ViewModel() {
+    private val recipeRepository = RecipeRepository()
+
+    private var recipeListener: ListenerRegistration? = null
+    private var _userRecipes = MutableLiveData<List<Recipe>>()
+    var userRecipes: LiveData<List<Recipe>> = _userRecipes
+
+    private var _searchedRecipes = MutableLiveData<List<Recipe>>()
+    var searchedRecipes = _searchedRecipes
+
+    private var _tagRecipes = MutableLiveData<List<Recipe>>()
+    var tagRecipes = _tagRecipes
+
+    private var selectedRecipeId: String? = null
+    private var _viewRecipe = MediatorLiveData<Recipe>()
+    var viewRecipe: LiveData<Recipe> = _viewRecipe
+
+    init {
+        _viewRecipe.addSource(_userRecipes) { recipes ->
+            selectedRecipeId?.let { id ->
+                _viewRecipe.value = recipes.find { it.recipeId == id }
+            }
+        }
+    }
+
+    fun createRecipe(userId: String?, recipe: Recipe? = null){
+        if (userId == null) {
+            android.util.Log.e("RecipeViewModel", "Failed to create recipe due to null userId")
+            return
+        }
+
+        val standardRecipe = recipe?.copy(
+            createdBy = userId
+        ) ?: Recipe(
+            name = "Unnamed",
+            createdBy = userId,
+        )
+
+        viewModelScope.launch {
+            val isSuccess = recipeRepository.addRecipe(
+                userId = userId,
+                recipe = standardRecipe
+            )
+            if (!isSuccess) {
+                android.util.Log.e("RecipeViewModel", "Failed to create recipe")
+            }
+        }
+    }
+
+    fun readRecipes(userId: String?) {
+        if (userId == null) return
+
+        recipeListener?.remove() // Stop previous listener if it exists
+
+        recipeListener = recipeRepository.listenForRecipes(userId) { recipes ->
+            _userRecipes.postValue(recipes)
+        }
+    }
+
+    fun searchRecipe(userId: String?, query: String) {
+        if (userId == null) return
+
+        viewModelScope.launch {
+            _searchedRecipes.value = recipeRepository.findRecipesByName(
+                userId = userId,
+                name = query
+            )
+
+            _tagRecipes.value = recipeRepository.findRecipesByTag(
+                userId = userId,
+                tag = query
+            )
+        }
+    }
+
+    fun selectRecipe(recipeId: String) {
+        selectedRecipeId = recipeId
+        _userRecipes.value?.let { recipes ->
+            _viewRecipe.value = recipes.find { it.recipeId == recipeId }
+        }
+    }
+
+    fun stopListening(){
+        recipeListener?.remove()
+        recipeListener = null
+    }
+
+    fun addTag(userId: String?, recipeId: String, tag: String){
+        if (userId == null) return
+
+        viewModelScope.launch {
+            val isSuccess = recipeRepository.addTag(
+                userId = userId,
+                recipeId = recipeId,
+                tag = tag
+            )
+            if (!isSuccess) {
+                android.util.Log.e("RecipeViewModel", "Failed to tag recipe")
+            }
+        }
+    }
+
+    fun updateTag(userId: String?, recipeId: String, oldTag: String, newTag: String){
+        if (userId == null) return
+
+        viewModelScope.launch {
+            val isSuccess = recipeRepository.updateTag(
+                userId = userId,
+                recipeId = recipeId,
+                oldTag = oldTag,
+                newTag = newTag
+            )
+            if (!isSuccess) {
+                android.util.Log.e("RecipeViewModel", "Failed to tag recipe")
+            }
+        }
+    }
+
+    fun deleteTag(userId: String?, recipeId: String, tag: String){
+        if (userId == null) return
+
+        viewModelScope.launch {
+            val isSuccess = recipeRepository.deleteTag(
+                userId = userId,
+                recipeId = recipeId,
+                tag = tag
+            )
+            if (!isSuccess) {
+                android.util.Log.e("RecipeViewModel", "Failed to untag recipe")
+            }
+        }
+    }
+
+    fun archiveRecipe(userId: String?, recipeId: String, context: Context){
+        if (userId == null) return
+
+        viewModelScope.launch {
+            val isSuccess = recipeRepository.archiveRecipe(
+                userId = userId,
+                recipeId = recipeId,
+            )
+            val message = if (isSuccess) "Recipe archived successfully!" else "Failed to archive recipe."
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun restoreRecipe(userId: String?, recipeId: String, context: Context){
+        if (userId == null) return
+
+        viewModelScope.launch {
+            val isSuccess = recipeRepository.restoreRecipe(
+                userId = userId,
+                recipeId = recipeId,
+            )
+            val message = if (isSuccess) "Recipe restored successfully!" else "Failed to restore recipe."
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun deleteRecipe(userId: String?, recipeId: String, context: Context){
+        if (userId == null) return
+
+        viewModelScope.launch {
+            val isSuccess = recipeRepository.deleteRecipe(
+                userId = userId,
+                recipeId = recipeId
+            )
+
+            val message = if (isSuccess) "Recipe deleted successfully!" else "Failed to delete recipe."
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
