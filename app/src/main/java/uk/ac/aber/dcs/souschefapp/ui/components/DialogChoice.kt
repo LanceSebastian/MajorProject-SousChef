a5fd115a7069fa3a70df3e37f63996729862e44c
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-04-21 04:34:40 +0100

    Refactor: Add customisable buttons
    The program can reuse it anywhere it different context

--- app/src/main/java/uk/ac/aber/dcs/souschefapp/ui/components/DialogChoice.kt
+++ app/src/main/java/uk/ac/aber/dcs/souschefapp/ui/components/DialogChoice.kt
@@ -7,12 +7,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -22,17 +19,12 @@ import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -40,13 +32,15 @@ import uk.ac.aber.dcs.souschefapp.ui.theme.AppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeAddDialogue(
+fun ChoiceDialogue(
     onDismissRequest: () -> Unit,
     mainAction: () -> Unit,
     secondAction: () -> Unit,
+    mainText: String = "main",
+    secondText: String = "second"
 ){
     var selectedIndex by remember { mutableIntStateOf(0) }
-    val options = listOf("Product", "Recipe")
+    val options = listOf(mainText, secondText)
 
     Dialog(
         onDismissRequest = onDismissRequest,
@@ -98,6 +92,7 @@ fun HomeAddDialogue(
                     Button(
                         onClick = {
                             if (selectedIndex == 0) mainAction() else secondAction()
+                            onDismissRequest()
                         }
                     ){
                         Text(
@@ -114,7 +109,7 @@ fun HomeAddDialogue(
 @Composable
 fun HomeAddDialoguePreview(){
     AppTheme(dynamicColor = false){
-        HomeAddDialogue(
+        ChoiceDialogue(
             onDismissRequest = {},
             mainAction = {},
             secondAction = {},
