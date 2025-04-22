fba8213001e534ebee32c125c8824605a5a14007
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-04-23 00:56:12 +0100

    Feat: Add Multiple Selections

--- app/src/main/java/uk/ac/aber/dcs/souschefapp/ui/components/MyCalendar.kt
+++ app/src/main/java/uk/ac/aber/dcs/souschefapp/ui/components/MyCalendar.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -97,49 +98,22 @@ fun MyCalendar(
 fun CustomCalendar(
     showDialog: Boolean,
     onDismiss: () -> Unit,
-    onDateSelected: (LocalDate) -> Unit,
+    onDateSelected: (LocalDate) -> Unit = {},
+    onDatesSelected: (List<LocalDate>) -> Unit = {},
     date: LocalDate = LocalDate.now(),
     logs: List<Log> = emptyList(),
+    isMany: Boolean = false
 ) {
-
-    val ratings = listOf(
-        Rating(
-            value = -2,
-            image = ImageVector.vectorResource(id = R.drawable.disappointed),
-            color = Color(0xFFF33B3B)
-        ),
-        Rating(
-            value = -1,
-            image = ImageVector.vectorResource(id = R.drawable.unhappy),
-            color = Color(0xFFFFB001)
-        ),
-        Rating(
-            value = 0,
-            image = ImageVector.vectorResource(id = R.drawable.neutral),
-            color = Color(0xFFCACA04)
-        ),
-        Rating(
-            value = 1,
-            image = ImageVector.vectorResource(id = R.drawable.happy),
-            color = Color(0xFF02C801)
-        ),
-        Rating(
-            value = 2,
-            image = ImageVector.vectorResource(id = R.drawable.satisfied),
-            color = Color(0xFF0095FF)
-        )
-    )
     if (!showDialog) return
 
     var selectedMonth by remember(date) {
-        mutableStateOf(
-            date.withDayOfMonth(1)
-                .let { YearMonth.from(it) }
-        )
+        mutableStateOf(date.withDayOfMonth(1).let { YearMonth.from(it) })
     }
 
     val today = LocalDate.now()
     var dateSelected by remember { mutableStateOf(date) }
+    val selectedDates = remember { mutableStateListOf<LocalDate>() }
+
     val daysInMonth = selectedMonth.lengthOfMonth()
     val firstDayOfWeek = selectedMonth.atDay(1).dayOfWeek.value % 7 // Sunday = 0
     val colorScheme = MaterialTheme.colorScheme
@@ -149,7 +123,11 @@ fun CustomCalendar(
         onDismissRequest = onDismiss,
         confirmButton = {
             TextButton(onClick = {
-                onDateSelected(dateSelected)
+                if (isMany) {
+                    onDatesSelected(selectedDates.sorted())
+                } else {
+                    onDateSelected(dateSelected)
+                }
                 onDismiss()
             }) {
                 Text("OK")
@@ -169,7 +147,6 @@ fun CustomCalendar(
                 IconButton(onClick = { selectedMonth = selectedMonth.minusMonths(1) }) {
                     Icon(Icons.AutoMirrored.Default.ArrowBack, contentDescription = "Previous Month")
                 }
-
                 Text(
                     text = selectedMonth.month.name.lowercase().replaceFirstChar { it.uppercase() } +
                             " ${selectedMonth.year}",
@@ -177,7 +154,6 @@ fun CustomCalendar(
                     modifier = Modifier.weight(1f),
                     textAlign = TextAlign.Center
                 )
-
                 IconButton(onClick = { selectedMonth = selectedMonth.plusMonths(1) }) {
                     Icon(Icons.AutoMirrored.Default.ArrowForward, contentDescription = "Next Month")
                 }
@@ -185,13 +161,11 @@ fun CustomCalendar(
         },
         text = {
             Column {
-                // Weekday headers
                 Row {
                     listOf("S", "M", "T", "W", "T", "F", "S").forEach {
                         Text(
                             text = it,
-                            modifier = Modifier
-                                .weight(1f),
+                            modifier = Modifier.weight(1f),
                             textAlign = TextAlign.Center
                         )
                     }
@@ -208,15 +182,23 @@ fun CustomCalendar(
                                 selectedMonth.atDay(dayIndex - firstDayOfWeek + 1)
                             } else null
 
+                            val isSelected = if (isMany) selectedDates.contains(date) else dateSelected == date
+                            val bgColor = when {
+                                isSelected -> dateColors[date] ?: colorScheme.primary
+                                else ->  Color.Transparent
+                            }
+                            val textColor = when {
+                                isSelected -> colorScheme.onPrimary
+                                today == date -> colorScheme.primary
+                                else -> colorScheme.onSurface
+                            }
+
                             Box(
                                 modifier = Modifier
                                     .weight(1f)
                                     .aspectRatio(1f)
                                     .padding(4.dp)
-                                    .background(
-                                        color = if(dateSelected == date) dateColors[date] ?: colorScheme.primary else Color.Transparent,
-                                        shape = CircleShape
-                                    )
+                                    .background(bgColor, shape = CircleShape)
                                     .border(
                                         width = 1.dp,
                                         color = dateColors[date] ?: Color.Transparent,
@@ -224,16 +206,20 @@ fun CustomCalendar(
                                     )
                                     .clickable(enabled = date != null) {
                                         date?.let {
-                                            dateSelected = it
-                                            onDateSelected(it)
+                                            if (isMany) {
+                                                if (selectedDates.contains(it)) {
+                                                    selectedDates.remove(it)
+                                                } else {
+                                                    selectedDates.add(it)
+                                                }
+                                            } else {
+                                                dateSelected = it
+                                            }
                                         }
                                     },
                                 contentAlignment = Alignment.Center
                             ) {
-                                Text(
-                                    text = date?.dayOfMonth?.toString() ?: "",
-                                    color = if (dateSelected == date) colorScheme.onPrimary else if (today == date) colorScheme.primary else colorScheme.onSurface
-                                )
+                                Text(text = date?.dayOfMonth?.toString() ?: "", color = textColor)
                             }
                         }
                     }
@@ -348,6 +334,63 @@ fun CustomCalendarView(){
             onDismiss = {},
             onDateSelected = {LocalDate.now()},
             logs = sampleLogs
+        )
+    }
+}
+
+@Preview
+@Composable
+fun RangeCustomCalendarView(){
+    val sampleLogs = listOf(
+        Log(
+            logId = "",
+            createdBy = "",
+            createdAt = Timestamp(Date(System.currentTimeMillis())),
+            rating = 2,
+            recipeIdList = listOf("1", "2"),
+            productIdList = listOf("0", "2"),
+            note = "Tried a new recipe, turned out great!"
+        ),
+        Log(
+            createdBy = "",
+            createdAt = Timestamp(Date(System.currentTimeMillis() - 86_400_000)), // 1 day ago
+            rating = -1,
+            recipeIdList = listOf("3"),
+            productIdList = listOf("3", "4"),
+            note = "Didn't like the taste, will try adjusting ingredients."
+        ),
+        Log(
+            createdBy = "",
+            createdAt = Timestamp(Date(System.currentTimeMillis() - 172_800_000)), // 2 days ago
+            rating = 1,
+            recipeIdList = listOf("6", "5"),
+            productIdList = listOf("5"),
+            note = "A decent meal, but could use more seasoning."
+        ),
+        Log(
+            createdBy = "",
+            createdAt = Timestamp(Date(System.currentTimeMillis() - 259_200_000)), // 3 days ago
+            rating = 0,
+            recipeIdList = emptyList(),
+            productIdList = listOf("5", "4"),
+            note = "Tried a new product, unsure about it yet."
+        ),
+        Log(
+            createdBy = "",
+            createdAt = Timestamp(Date(System.currentTimeMillis() - 345_600_000)), // 4 days ago
+            rating = -2,
+            recipeIdList = listOf("4"),
+            productIdList = listOf("2", "3"),
+            note = "Had a bad experience with this recipe."
+        )
+    )
+    AppTheme{
+        CustomCalendar(
+            showDialog = true,
+            onDismiss = {},
+            onDatesSelected = {LocalDate.now()},
+            logs = sampleLogs,
+            isMany = true
         )
     }
 }
